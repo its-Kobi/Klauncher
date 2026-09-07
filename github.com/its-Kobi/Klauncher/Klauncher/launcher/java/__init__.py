1e654083a515424e56312532a0a0f582d848439e
Kobi_ <bogiwagahody@gmail.com>
2026-09-07 23:52:49 +0100

    unzipgit

---
+++ github.com/its-Kobi/Klauncher/Klauncher/launcher/java/__init__.py
@@ -0,0 +1,3 @@
+from launcher.java.manager import JavaManager
+from launcher.java.matcher import required_java_for_version, java_compatible
+__all__ = ["JavaManager", "required_java_for_version", "java_compatible"]
